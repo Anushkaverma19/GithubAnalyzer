a77382d04df6c3a0cd28cc69726814ca21313352
unknown <10anushka2023@gmail.com>
2026-06-19 10:49:19 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,15 +9,21 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false,
+
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
 const connectMySQL = async () => {
   try {
     await sequelize.authenticate();
-    console.log("MySQL Connected");
+    console.log("MySQL Connected ✅");
   } catch (err) {
-    console.log("DB Error:", err);
+    console.log("DB Error ❌:", err);
   }
 };
 
